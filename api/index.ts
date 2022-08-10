666d0ba24fbd66c68fd813ff7e363aee4f425d57
neweos <65535758+neweos@users.noreply.github.com>
2022-08-10 14:04:45 +0800

    neweos created a commit :sparkles:

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { weather, keyAuth } from '@tossdev/weather'
 
 async function handler(request: VercelRequest, response: VercelResponse) {
